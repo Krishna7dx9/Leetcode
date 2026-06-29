7981ce60ca7a8d6ae10f9630b540d8a8d4a60dc0
Krishna Sharma <158446402+Krishna7dx9@users.noreply.github.com>
2026-06-29 14:42:21 +0530

    feat: update LeetCode 567. Permutation in String solution (python)

--- problems/permutation-in-string/solution.py
+++ problems/permutation-in-string/solution.py
@@ -1,25 +1,53 @@
 class Solution(object):
     def checkInclusion(self, s1, s2):
 
-        freq1 = {}
-        freq_substring = {}
-        freq_substring[s2[0]] = freq_substring.get(s2[0], 0) + 1
+        if len(s1) > len(s2):
+            return False
+
         end = 1
+        matches = 0
+        arr1 = [0]*26
+        window = [0] *26
+        window[ord(s2[0]) - ord('a')] += 1
 
         for ch in s1:
-            freq1[ch] = freq1.get(ch, 0) + 1
+            arr1[ord(ch) - ord('a')] += 1
+
+        # Build first complete window
+        while end <= len(s1) - 1:
+            index = ord(s2[end]) - ord('a')
+            window[index] += 1
+            end += 1
+
+        # Initial matches
+        for i in range(26):
+            if arr1[i] == window[i]:
+                matches += 1
 
         for start in range(len(s2) - len(s1) + 1):
 
-            while end <= start + len(s1) - 1:
-                freq_substring[s2[end]] = freq_substring.get(s2[end], 0) + 1
-                end += 1
-
-            if freq_substring == freq1:
+            if matches == 26:
                 return True
 
-            freq_substring[s2[start]] -= 1
-            if freq_substring[s2[start]] == 0:
-                del freq_substring[s2[start]]
+            while end <= start + len(s1) - 1:
+                index = ord(s2[end]) - ord('a')
+                window[index] += 1
+                end += 1
+
+                if window[index] == arr1[index]:
+                    matches += 1
+                elif window[index] == arr1[index] + 1:
+                    matches -= 1
+
+            if matches == 26:
+                return True
+
+            index = ord(s2[start]) - ord('a')
+            window[index] -= 1
         
+            if window[index] == arr1[index]:
+                matches += 1
+            elif window[index] == arr1[index] - 1:
+                matches -= 1
+
         return False
