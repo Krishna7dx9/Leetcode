e36df8b9b1eba829d294f00beffffd10761a8d14
Krishna Sharma <158446402+Krishna7dx9@users.noreply.github.com>
2026-07-02 14:18:36 +0530

    feat: update LeetCode 239. Sliding Window Maximum solution (python)

--- problems/sliding-window-maximum/solution.py
+++ problems/sliding-window-maximum/solution.py
@@ -1,11 +1,31 @@
+from collections import deque
+
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
+
         max_array = []
 
-        for left in range(len(nums) - k + 1):
-            right = left + k
-            window = nums[left: right]
-            max_array.append(max(window))
+        # store indices of useful maximum candidates
+        window = deque()
+
+        for right in range(len(nums)):
+
+            # Remove smaller elements from back
+            # They can never become future maximum
+            while window and nums[window[-1]] < nums[right]:
+                window.pop()
+
+            # Add current index
+            window.append(right)
+
+            # Remove front if it left current window
+            if window[0] <= right - k:
+                window.popleft()
+
+            # Window complete
+            if right >= k - 1:
+
+                # Front always stores maximum index
+                max_array.append(nums[window[0]])
 
         return max_array
-  
